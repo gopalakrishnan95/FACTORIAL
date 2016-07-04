d4ae530dab74258da80f8ea6d512bb6de3ba5ad7
Gopala krishnan H <gopalakrishnan.hari95@gmail.com>
2016-07-04 16:26:02 +0530

    Create revers word.c

---
+++ revers word.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char a[20];
+printf("\n enter the word:");
+scanf("%s",a);
+{
+strrev(a);
+printf("%s",a);
+}
+getch();
+}
+
+
+
